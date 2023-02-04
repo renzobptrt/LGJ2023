9bff1526e251ae25b6a67e5bcbd3cc5b390dc70d
Coralain <carolain.antoc@gmail.com>
2023-02-04 07:09:48 -0500

    falling objs to UI

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,23 +8,23 @@ public class Spawner : MonoBehaviour
     [SerializeField] float bound;
     [SerializeField] float yTop;
     [SerializeField] bool won = false;
-    float speed = 3f;
+    float speed = 64f;
 
     void Awake()
     {
-        float camHalfHeight = Camera.main.orthographicSize;
-        float camHalfWidth = Camera.main.aspect * camHalfHeight;
-        Vector3 bSize = blocksPrefabs[0].GetComponent<Renderer>().bounds.size;
-        bound = camHalfWidth - (bSize.x/2);
-        yTop = camHalfHeight - (bSize.y/2);
-        transform.position = new Vector3(0, yTop, 0);
+        // float camHalfHeight = Camera.main.orthographicSize;
+        // float camHalfWidth = Camera.main.aspect * camHalfHeight;
+        // Vector3 bSize = blocksPrefabs[0].GetComponent<Renderer>().bounds.size;
+        // bound = camHalfWidth - (bSize.x/2);
+        // yTop = camHalfHeight - (bSize.y/2);
+        Vector2 currSize = GetComponent<RectTransform>().sizeDelta;
+        Vector2 bSize = blocksPrefabs[0].GetComponent<RectTransform>().sizeDelta;
+        bound = (Screen.width + currSize.x - bSize.x)/2;
+        yTop = (Screen.height + currSize.y - bSize.y)/2;
+        // transform.position = new Vector3(0, yTop, 0);
+
         StartCoroutine( Spawn() );
     }
-
-    // void Update()
-    // {
-        
-    // }
 
     IEnumerator Spawn()
     {
@@ -32,10 +32,10 @@ public class Spawner : MonoBehaviour
         {
             int typeBlock = Random.Range(0,blocksPrefabs.Count);
             GameObject block = blocksPrefabs[typeBlock];
+            block = Instantiate(block, Vector2.zero, block.transform.rotation, gameObject.transform);
             block.GetComponent<Serpent>().speed = speed;
             float xPos = Random.Range(-bound,bound);
-            Vector3 pos = new Vector3( xPos, yTop, 0);
-            Instantiate(block, pos, block.transform.rotation, gameObject.transform);
+            block.GetComponent<RectTransform>().localPosition = new Vector3( xPos, yTop, 0);
             float t = Random.Range(1,2);
             yield return new WaitForSeconds(t);
         }
