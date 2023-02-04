ca103df1ed1bbe0a0adf9f5d4e6bc5ccc39b5905
renzobptrt <renzobptrt@gmail.com>
2023-02-04 13:18:34 -0500

    Counter to start level

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -3,39 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using TMPro;
+using static Callbacks;
 
 public class GameManager : MonoBehaviour
 {
     
     //Elements
     public Button m_ButtonLestGo = null;
+    [SerializeField] private TextMeshProUGUI m_TextCount = null;
 
     void Start()
     {
         m_ButtonLestGo.onClick.RemoveAllListeners();
         m_ButtonLestGo.onClick.AddListener(StartCanvaReady);
+        canvas =  m_PanelUI.GetComponent<CanvasGroup>();
         //StartCanvaReady();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void StartCanvaReady()
     {
-        CanvasGroup canvas = m_PanelUI.GetComponent<CanvasGroup>();
-        canvas.blocksRaycasts = true;
-        canvas.DOFade(0f,0.5f).OnComplete(()=>{
+        //Transform parent = m_TextToShowReference.transform.parent.transform.parent;
+        //parent.GetComponent<RectTransform>().DOAnchorPosY(200,4f,true).SetDelay(2f);
+        canvas.DOFade(1f,0.5f).OnComplete(()=>{
             canvas.blocksRaycasts = false;
+            AnimationCount(3);
         });
+    }
+
+    void AnimationCount(int numnber)
+    {
+        if(numnber>0)
+        {
+            m_TextCount.text = numnber.ToString();
+            m_TextCount.transform.GetComponent<RectTransform>().DOScale(1,0.5f).OnComplete(()=>{
+                m_TextCount.transform.GetComponent<RectTransform>().DOScale(0,0.5f).OnComplete(()=>{
+                    numnber--;
+                    AnimationCount(numnber);
+                });
+            });
+        }
+        else{
+            canvas.blocksRaycasts = true;
+            canvas.DOFade(0f,0.5f).OnComplete(()=>{
+                canvas.blocksRaycasts = false;
+            });
+        }
     }
 
     void StartGameSubLevel()
     {
-
+        canvas.blocksRaycasts = true;
+        canvas.DOFade(1f,0.5f).OnComplete(()=>{
+            m_ButtonLestGo.interactable = true;
+        });
     }
 
-    [SerializeField] private RectTransform m_PanelUI = null;
+    public RectTransform m_PanelUI = null;
+    public CanvasGroup canvas = null;
 }
