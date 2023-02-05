c8abf7ec48395a47becb4c01abb070d00765904d
Coralain <carolain.antoc@gmail.com>
2023-02-05 03:13:49 -0500

    narattive edit

--- Assets/Scripts/NarrativeManager.cs
+++ Assets/Scripts/NarrativeManager.cs
@@ -1,34 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public class NarrativeManager : MonoBehaviour
 {
-    public List<Scene> scenes;
     [SerializeField] int currentScene = 0;
+    [SerializeField] Image background;
+    [SerializeField] Image character;
+    [SerializeField] TextMeshProUGUI tmpro;
+    public List<Scene> scenes;
     public float timePerDialog = 3;
+    public bool didCurrSceneEnd = false;
 
     void Awake()
     {
+        background = GetComponent<Image>();
+        ShowDialog();
+    }
+
+    void ShowDialog()
+    {
+        didCurrSceneEnd = false;
+        if(scenes[currentScene].background) background.sprite = scenes[currentScene].background;
+        if(scenes[currentScene].character) character.sprite = scenes[currentScene].character;
+        gameObject.SetActive(true);
         StartCoroutine( NextDialog() );
     }
 
     IEnumerator NextDialog()
     {
-        yield return new WaitForSeconds(timePerDialog);
+        foreach (string dialog in scenes[currentScene].dialogs)
+        {
+            tmpro.text = scenes[currentScene].charname + "\n" + dialog; 
+            yield return new WaitForSeconds(timePerDialog);
+        }
+        currentScene ++;
+        didCurrSceneEnd = true;
     }
 
-    void ShowDialog()
-    {
-        
-    }
 
     [System.Serializable]
     public class Scene
     {
-        public Image background;
-        public Image character;
+        public Sprite background;
+        public Sprite character;
         public string charname;
         public List<string> dialogs;
     }
